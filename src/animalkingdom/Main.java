d171358b68cb25cf04c2715410f3c5e7454d9845
Caleb <bargercj20@hotmail.com>
2020-06-03 12:53:23 -0700

    ok instantiated the classes as well

--- src/animalkingdom/Main.java
+++ src/animalkingdom/Main.java
@@ -2,6 +2,8 @@ package animalkingdom;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Mammals
         Mammal panda = new Mammal("Panda", 1869);
         Mammal zebra = new Mammal("Zebra", 1778);
         Mammal koala = new Mammal("Koala", 1816);
@@ -10,10 +12,16 @@ public class Main {
         Mammal raccoon = new Mammal("Raccoon", 1758);
         Mammal bigfoot = new Mammal("Bigfoot", 2021);
 
-        
+        // Birds
+        Bird pigeon = new Bird("Pigeon", 1837);
+        Bird peacock = new Bird("Peacock", 1821);
+        Bird toucan = new Bird("Toucan", 1758);
+        Bird parrot = new Bird("Parrot", 1824);
+        Bird swan = new Bird("Swan", 1758);
 
-
-
-        System.out.println(panda.toString());
+        // Fish
+        Fish salmon = new Fish("Salmon", 1758);
+        Fish catfish = new Fish("Catfish", 1817);
+        Fish perch = new Fish("Perch", 1758);
     }
 }
